a3686f912bf4a86d0d44b68345abd3ae8c4c1585
José Juan Ramos <josejuan.ramoscardenas@gmail.com>
2023-01-06 09:17:32 -0600

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=b24b3b7abad505e6f61fb7d6d87bb856&query='+ latitude + ',' + longitude
+
+    request({url: url, json:true}, (error, response) => {
+        if (error){
+            callback('Unable to connect to weather service', undefined)
+        } else if (response.body.error){
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + '% chance of rain')
+        }
+    })
+}
+
+module.exports = forecast
